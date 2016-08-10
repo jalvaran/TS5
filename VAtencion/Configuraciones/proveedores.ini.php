bc34cb9a17b1e42cee9d050ed5d283c40dff33b0
jalvaran <jalvaran@gmail.com>
2016-08-09 22:23:44 -0500

    Se agrega Bug de codigos de los municipios y paises en clientes y proveedores

--- VAtencion/Configuraciones/proveedores.ini.php
+++ VAtencion/Configuraciones/proveedores.ini.php
@@ -24,22 +24,24 @@ $Vector["Limit"]=$limit;            //Numero de Registros a mostrar
 $Vector["VerRegistro"]["Deshabilitado"]=1; 
 ///Columnas excluidas
 
-
-
 $Vector["Cod_Dpto"]["Vinculo"]=1;   //Indico que esta columna tendra un vinculo
-$Vector["Cod_Dpto"]["TablaVinculo"]="cod_departamentos";  //tabla de donde se vincula
-$Vector["Cod_Dpto"]["IDTabla"]="Cod_dpto"; //id de la tabla que se vincula
-$Vector["Cod_Dpto"]["Display"]="Nombre";//Columna que quiero mostrar
-//
+$Vector["Cod_Dpto"]["TablaVinculo"]="cod_municipios_dptos";  //tabla de donde se vincula
+$Vector["Cod_Dpto"]["IDTabla"]="Cod_Dpto"; //id de la tabla que se vincula
+$Vector["Cod_Dpto"]["Display"]="Departamento";//Columna que quiero mostrar
+$Vector["Cod_Dpto"]["Predeterminado"]="76";
+
+//$Vector["Excluir"]["Cod_Mcipio"]=1;
 $Vector["Cod_Mcipio"]["Vinculo"]=1;   //Indico que esta columna tendra un vinculo
 $Vector["Cod_Mcipio"]["TablaVinculo"]="cod_municipios_dptos";  //tabla de donde se vincula
 $Vector["Cod_Mcipio"]["IDTabla"]="Cod_mcipio"; //id de la tabla que se vincula
 $Vector["Cod_Mcipio"]["Display"]="Ciudad"; 
+$Vector["Cod_Mcipio"]["Predeterminado"]="";
 
 $Vector["Pais_Domicilio"]["Vinculo"]=1;   //Indico que esta columna tendra un vinculo
 $Vector["Pais_Domicilio"]["TablaVinculo"]="cod_paises";  //tabla de donde se vincula
 $Vector["Pais_Domicilio"]["IDTabla"]="Codigo"; //id de la tabla que se vincula
 $Vector["Pais_Domicilio"]["Display"]="Pais"; 
+$Vector["Pais_Domicilio"]["Predeterminado"]="169";
 
 $Vector["Tipo_Documento"]["Vinculo"]=1;   //Indico que esta columna tendra un vinculo
 $Vector["Tipo_Documento"]["TablaVinculo"]="cod_documentos";  //tabla de donde se vincula
