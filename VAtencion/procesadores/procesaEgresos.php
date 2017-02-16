1f11625b979a6ae019dc2bbca51b48026775b0b0
JULIANPORTATIL <JULIANPORTATIL@PORTATILJAAV>
2017-02-16 09:44:01 -0500

    Correccion en la foto de un egreso

--- VAtencion/procesadores/procesaEgresos.php
+++ VAtencion/procesadores/procesaEgresos.php
@@ -41,7 +41,7 @@
                 //echo "<script>alert ('entra')</script>";
 		if(!empty($_FILES['foto']['name'])){
                     //echo "<script>alert ('entra foto')</script>";
-			$carpeta="../SoportesEgresos/";
+			$carpeta="SoportesEgresos/";
 			opendir($carpeta);
                         $Name=str_replace(' ','_',$_FILES['foto']['name']);  
 			$destino=$carpeta.$Name;
